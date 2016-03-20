edd927d903963d4145219b2cce48e10cb66ecbbe
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2016-03-20 10:15:43 +0530

    feat(Zigzag.each/3)

--- lib/zigzag.ex
+++ lib/zigzag.ex
@@ -1,2 +1,29 @@
 defmodule Zigzag do
+  @doc """
+  Applies `fun` to each item of `list` in parallel, running at most `limit`
+  processes at any given time.
+
+  ## Examples
+
+      iex> Zigzag.each([1, 2, 3], 3, fn _ -> nil end)
+      :ok
+  """
+  def each(list, limit, fun) do
+    each(list, limit, fun, 0)
+  end
+
+  defp each([], _limit, _fun, 0), do: :ok
+  defp each([h|t], limit, fun, running) when running < limit do
+    me = self
+    spawn_link(fn ->
+      fun.(h)
+      send me, :cont
+    end)
+    each(t, limit, fun, running + 1)
+  end
+  defp each(list, limit, fun, running) when is_list(list) do
+    receive do
+      :cont -> each(list, limit, fun, running - 1)
+    end
+  end
 end
--- test/zigzag_test.exs
+++ test/zigzag_test.exs
@@ -2,7 +2,21 @@ defmodule ZigzagTest do
   use ExUnit.Case
   doctest Zigzag
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "each/3" do
+    total = 11
+    limit = 5
+    {:ok, ran} = Agent.start_link(fn -> 0 end)
+    {:ok, running} = Agent.start_link(fn -> 0 end)
+    {time, :ok} = :timer.tc fn ->
+      Zigzag.each Enum.to_list(1..total), limit, fn i ->
+        Agent.update(running, &(&1 + 1))
+        assert Agent.get(running, &(&1)) <= limit
+        :timer.sleep(i * 10)
+        Agent.update(ran, &(&1 + 1))
+        Agent.update(running, &(&1 - 1))
+      end
+    end
+    assert time < 20 * 10 * 1000
+    assert Agent.get(ran, &(&1)) == total
   end
 end
